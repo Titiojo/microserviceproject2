073338f572ebca02f31b6536e1486a6d4421900e
Titiojo <adewumi.titi@gmail.com>
2024-10-09 15:08:48 -0400

    first commit

---
+++ Terraform-jenkins-server/backend.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+  }
+
+
+
+  backend "s3" {
+
+    bucket  = "newprojectt"    //manually created 
+    key     = "jenkins/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+
+    # dynamodb_table = "Terraform_lock"   //manually created 
+
+  }
+
+}
